d25a32a90f00fc876c6ddcd5491e33de187d38b2
ochieng24 <edwine.ochi24@gmail.com>
2023-04-25 13:12:14 -0700

    hot fix

--- func1.c
+++ func1.c
@@ -17,7 +17,7 @@ int print_unsigned(va_list types, char buffer[],
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 
-	num = convert_size_unsgnd(num,s);
+	num = convert_size_unsgnd(num, s);
 
 	if (num == 0)
 		buffer[i--] = '0';
